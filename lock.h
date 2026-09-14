47f9188580444504f5a21f57710f8447deeaa0e8
AkshaySabharwal <akshaysabharwal2007@gmail.com>
2026-09-15 04:50:33 +0530

    Update lock.h: Add WHERE conditions, Multi-table JOIN, and ACID Transactions

--- lock.h
+++ lock.h
@@ -1,14 +1,28 @@
 #ifndef LOCK_H
 #define LOCK_H
 
+#define MAX_LOCKS 1000
+
+typedef enum {
+    LOCK_NONE,
+    LOCK_SHARED,    // S-Lock (Read)
+    LOCK_EXCLUSIVE  // X-Lock (Write)
+} LockMode;
+
 typedef struct {
     int recordId;
     int locked;
+    LockMode mode;
+    int ownerTxId;
 } Lock;
 
-extern Lock lockTable[100];
+extern Lock lockTable[MAX_LOCKS];
 
+void initLockManager(void);
 int acquireLock(int id);
+int acquireLockEx(int id, LockMode mode, int txId);
 void releaseLock(int id);
+void releaseTxLocks(int txId);
+void printLockStatus(void);
 
 #endif
