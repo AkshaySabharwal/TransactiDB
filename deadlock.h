6effb6e004eef88983b2216adeae6b9428dc3b56
AkshaySabharwal <akshaysabharwal2007@gmail.com>
2026-09-15 04:50:36 +0530

    Update deadlock.h: Add WHERE conditions, Multi-table JOIN, and ACID Transactions

--- deadlock.h
+++ deadlock.h
@@ -1,7 +1,12 @@
 #ifndef DEADLOCK_H
 #define DEADLOCK_H
 
+#define MAX_NODES 32
+
+void resetGraph(void);
 void addEdge(int from, int to);
-int detectDeadlock();
+void removeEdge(int from, int to);
+int detectDeadlock(void);
+void printWaitGraph(void);
 
 #endif
