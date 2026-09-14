35ad90a3576b19849b061efd84aadfb56fe21b0e
AkshaySabharwal <akshaysabharwal2007@gmail.com>
2026-09-15 04:50:22 +0530

    Update index.h: Add WHERE conditions, Multi-table JOIN, and ACID Transactions

--- index.h
+++ index.h
@@ -1,9 +1,22 @@
 #ifndef INDEX_H
 #define INDEX_H
 
+#define HASH_SIZE 1031
+
+typedef struct HashIndex {
+    int hashKeys[HASH_SIZE];
+    int hashVals[HASH_SIZE];
+} HashIndex;
+
 void indexInit(void);
 int indexInsert(int id, int rowPos);
 int indexSearch(int id);
 int indexDelete(int id);
+
+// Per-table hash index API
+void tableIndexInit(HashIndex* idx);
+int tableIndexInsert(HashIndex* idx, int id, int rowPos);
+int tableIndexSearch(const HashIndex* idx, int id);
+int tableIndexDelete(HashIndex* idx, int id);
 
 #endif
